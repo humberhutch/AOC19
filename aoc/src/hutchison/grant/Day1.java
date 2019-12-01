a43d8cb07fe26f9433651cce538a5e7d24aaaa80
G Hutchison <grant.hutchison@tdsb.on.ca>
2019-12-01 09:32:17 -0500

    day 1 complete

---
+++ aoc/src/hutchison/grant/Day1.java
@@ -0,0 +1,50 @@
+package hutchison.grant;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day1 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		part2();
+	}
+
+	public static void part1() throws FileNotFoundException {
+		File f = new File("day1.txt");
+		Scanner in = new Scanner(f);
+
+		int totalFuel = 0;
+		while (in.hasNextLine()) {
+			String data = in.nextLine().trim();
+			int num = Integer.parseInt(data);
+
+			// divide by 3 and round down to get 4, then subtract 2 to get 2.
+			totalFuel += Math.floor(num / 3) - 2;
+
+		}
+		System.out.println(totalFuel);
+
+	}
+
+	public static void part2() throws FileNotFoundException {
+		File f = new File("day1.txt");
+		Scanner in = new Scanner(f);
+
+		int totalFuel = 0;
+		while (in.hasNextLine()) {
+			String data = in.nextLine().trim();
+			int num = Integer.parseInt(data);
+
+			// divide by 3 and round down to get 4, then subtract 2 .
+			int nextFuel = (int) Math.floor(num / 3) - 2; 
+			int subTotal = 0;
+			while (nextFuel>=0) {
+				subTotal += nextFuel;
+				nextFuel = (int) Math.floor(nextFuel / 3) - 2;
+			}
+			totalFuel += subTotal;
+		}
+		System.out.println(totalFuel);
+	}
+}
